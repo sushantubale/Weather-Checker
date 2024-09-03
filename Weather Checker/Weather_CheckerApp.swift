ab14252eac06d71226ab84f011f36b3ba4e13eda
Sushant Ubale <sushantubale@gmail.com>
2024-09-03 09:39:26 -0500

    Initial Commit

---
+++ Weather Checker/Weather_CheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_CheckerApp.swift
+//  Weather Checker
+//
+//  Created by Sushant Ubale on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
